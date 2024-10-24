8dd37dd2abb5d9817cc61483944c45744a6e52f0
Daniel Bar <98904765+DanielBar2145658@users.noreply.github.com>
2024-10-24 18:13:17 -0400

    GED

---
+++ Source/GalacticGauntlet/Public/GameManagerSubsystem.h
@@ -0,0 +1,18 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/Subsystem.h"
+#include "GameManagerSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GALACTICGAUNTLET_API UGameManagerSubsystem : public USubsystem
+{
+	GENERATED_BODY()
+
+	
+};
